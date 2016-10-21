3734ab7adea8539d903be84ed607cc57b2d3af7e
Sergey Matveev <stargrave@stargrave.org>
2016-10-21 09:39:07 +0300

    More language neutral quotes

--- www.mk
+++ www.mk
@@ -9,4 +9,6 @@ gogost.html: www.texi
 		--set-customization-variable SHOW_TITLE=0 \
 		--set-customization-variable DATE_IN_HEADER=1 \
 		--set-customization-variable TOP_NODE_UP_URL=index.html \
+		--set-customization-variable CLOSE_QUOTE_SYMBOL=\" \
+		--set-customization-variable OPEN_QUOTE_SYMBOL=\" \
 		-o gogost.html www.texi

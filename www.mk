05a181717085c2ce9b6d474d4252c08e0c9bbdb4
Sergey Matveev <stargrave@stargrave.org>
2016-11-19 23:17:54 +0300

    No access keys during website generation

--- www.mk
+++ www.mk
@@ -7,6 +7,7 @@ gogost.html: www.texi
 	$(MAKEINFO) --html \
 		--set-customization-variable NO_CSS=1 \
 		--set-customization-variable SHOW_TITLE=0 \
+		--set-customization-variable USE_ACCESSKEY=0 \
 		--set-customization-variable DATE_IN_HEADER=1 \
 		--set-customization-variable TOP_NODE_UP_URL=index.html \
 		--set-customization-variable CLOSE_QUOTE_SYMBOL=\" \

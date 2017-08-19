972a6bab732ca7b23f098fffbbb972edfea68398
Sergey Matveev <stargrave@stargrave.org>
2017-08-19 16:54:52 +0300

    Change licence to LGPLv3+

--- src/cypherpunks.ru/gogost/gost28147/cipher.go
+++ src/cypherpunks.ru/gogost/gost28147/cipher.go
@@ -2,17 +2,18 @@
 // Copyright (C) 2015-2017 Sergey Matveev <stargrave@stargrave.org>
 //
 // This program is free software: you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation, either version 3 of the License, or
-// (at your option) any later version.
+// it under the terms of the GNU Lesser General Public License as
+// published by the Free Software Foundation, either version 3 of the
+// License, or (at your option) any later version.
 //
 // This program is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU General Public License for more details.
+// GNU Lesser General Public License for more details.
 //
-// You should have received a copy of the GNU General Public License
-// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+// You should have received a copy of the GNU Lesser General Public
+// License along with this program.  If not, see
+// <http://www.gnu.org/licenses/>.
 
 // GOST 28147-89 block cipher with ECB, CFB, CTR, MAC modes of operation.
 // RFC 5830.
